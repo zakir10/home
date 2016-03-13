95268f30bcc623ad62aed8c70671e66b35f642c8
Zakir Ahmed <Zakir@Zakirs-iMac.home>
2016-03-13 17:28:24 +0000

    added scan function for 2 values addition

--- maths_functions.c
+++ maths_functions.c
@@ -8,6 +8,15 @@ int main()
 	printf("Substract 7-2= %i\n", 7-2);
 	printf("Multiply 5+5= %i\n", 5*5);
 	printf("Divide 15/3= %i\n", 15/3);
+	
+	
+	int a, b, c;
+ 
+   printf("Enter two numbers to add\n");
+   scanf("%d%d",&a,&b);
+   
+   c = a + b;
 
+   printf("%d\n",c);
 
 }
